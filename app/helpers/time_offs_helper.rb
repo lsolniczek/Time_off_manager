372feb0fbbd167dd87d322a681d15687bfe25731
LukSol <l.solniczek@gmail.com>
2013-11-18 23:05:16 +0100

    Add status helper

--- app/helpers/time_offs_helper.rb
+++ app/helpers/time_offs_helper.rb
@@ -1,2 +1,18 @@
 module TimeOffsHelper
+
+	def time_off_state_ico(single_time_off)
+		case single_time_off.state
+			when "new"
+				content_tag(:p, "Nowy", class: "label label-primary")
+			when "in_approval"
+				content_tag(:p, "W akceptacji", class: "label label-warning")
+			when "accepted"
+				content_tag(:p, "Zaakceptowany", class: "label label-success")
+			when "rejected"
+				content_tag(:p, "Odrzucony", class: "label label-danger")
+			else
+				content_tag(:p, "Anulowany", class: "label label-default")
+		end
+	end
+
 end
